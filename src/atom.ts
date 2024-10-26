30ff89a43dcc295973237bc9e73511c24901e9e7
Gabriel Cozma <me@gabs.eu.org>
2024-10-26 15:12:43 +0300

    feat(atom): make content optional

--- src/atom.ts
+++ src/atom.ts
@@ -7,7 +7,7 @@ interface AtomEntry {
   updated: Date;
   image?: string;
   summary: string;
-  content: {
+  content?: {
     body: string;
     type?: string;
   };
@@ -57,8 +57,8 @@ export class AtomFeed extends BaseFeed<AtomEntry> {
         `    <id>${escapeXml(entry.id)}</id>\n`,
         `    <updated>${entry.updated.toISOString()}</updated>\n`,
         `    <summary>${escapeXml(entry.summary)}</summary>\n`,
-        `    <content type="${entry.content.type || "text"}">${
-          escapeXml(entry.content.body || entry.summary)
+        `    <content type="${entry.content?.type || "text"}">${
+          escapeXml(entry.content?.body || entry.summary)
         }</content>\n`,
         entry.image
           ? `    <media:thumbnail url="${escapeXml(entry.image)}" />\n`
