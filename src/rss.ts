243ff2694eb3a6da95a154327d5cf0bf46e1127f
Gabriel Cozma <me@gxbs.dev>
2026-04-05 22:06:11 +0300

    fix: error in date for rss

--- src/rss.ts
+++ src/rss.ts
@@ -51,6 +51,7 @@ export class RssFeed extends BaseFeed<RssItem> {
         const emailPart = escapedEmail ? `${escapedEmail} ` : "";
         return (
           `    <webMaster>${emailPart}(${escapedName})</webMaster>\n` +
+          `    <author>${emailPart}(${escapedName})</author>\n` +
           `    <managingEditor>${emailPart}(${escapedName})</managingEditor>\n`
         );
       }).join("");
